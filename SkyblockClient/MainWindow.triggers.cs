0f0e163d41fa3b7464a47d5840d183778fe3e466
Feel65 <69548576+Feel65@users.noreply.github.com>
2021-06-21 09:37:14 -1000

    delete this

--- SkyblockClient/MainWindow.triggers.cs
+++ SkyblockClient/MainWindow.triggers.cs
@@ -55,20 +55,7 @@ namespace SkyblockClient
 			}
 			*/
 		}
-
-		private async void BtnUpdate_Click(object sender, RoutedEventArgs e)
-		{
-			if (!Globals.Settings.enableUpdateButton)
-			{
-				Utils.Info("The update function is temporarily unavailable");
-				Globals.ShowInfo("The update function is temporarily unavailable", "Info");
-				return;
-			}
-			ButtonsEnabled(false);
-			await Startupdate();
-			ButtonsEnabled(true);
-		}
-
+		
         private async void BtnInstallModsAndForgeClick(object sender, RoutedEventArgs e)
 		{
 			ButtonsEnabled(false);
